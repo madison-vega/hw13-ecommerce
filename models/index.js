c233299683c96e43ff1144a7db7830e3aa8f1c1f
Madison Vega <madison.r.vega@gmail.com>
2021-04-26 14:45:08 -0500

    added relationships between models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
 
-// Tags belongToMany Products (through ProductTag)
+})
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+
+})
+
+Product.hasMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+
+})
+
 
 module.exports = {
   Product,
